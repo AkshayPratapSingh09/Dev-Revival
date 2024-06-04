78a019743ab182a447733b0439209652c51282d3
Akshay <worklikeakshay@gmail.com>
2024-06-04 23:57:25 +0530

    Added day 10

---
+++ Day_10/first.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+technologies   = ({
+    'Courses':["Spark","PySpark","Hadoop","Python","Pandas",None,"Spark","Python"],
+    'Fee' :[22000,25000,23000,24000,np.nan,25000,25000,22000],
+    'Duration':['30day','50days','55days','40days','60days','35day','','50days'],
+    'Discount':[1000,2300,1000,1200,2500,1300,1400,1600]
+          })
+row_labels=['r0','r1','r2','r3','r4','r5','r6','r7']
+df = pd.DataFrame(technologies, index=row_labels)
+print(df)
