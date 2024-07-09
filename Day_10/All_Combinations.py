2de0b77cfdae665a2cd1b3e7635b5d2028b55f5a
Akshay <worklikeakshay@gmail.com>
2024-07-09 23:07:50 +0530

    Added day 10

--- Day_10/All_Combinations.py
+++ Day_10/All_Combinations.py
@@ -20,16 +20,16 @@ print(ans)
 print(ans)
     
 
-# def solve(nums,output,index,ans):
-#     if (index >= len(nums)):
-#         ans.append(output)
-#         # print(ans)
-#         return
+def solve(nums,output,index,ans):
+    if (index >= len(nums)):
+        ans.append(output)
+        # print(ans)
+        return
     
-#     solve(nums,output,index+1,ans)
+    solve(nums,output,index+1,ans)
 
-#     element = nums[index]
-#     output.append(element)
-#     solve(nums,output,index+1,ans)
+    element = nums[index]
+    output.append(element)
+    solve(nums,output,index+1,ans)
 
-# print(solve([],[],0,ans))
+print(solve([],[],0,ans))
