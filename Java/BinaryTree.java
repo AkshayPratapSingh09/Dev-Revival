b559141d454d4cad7fd149cca64706403e48861c
Akshay <worklikeakshay@gmail.com>
2024-07-17 23:53:56 +0530

    Added more tree

--- Java/BinaryTree.java
+++ Java/BinaryTree.java
@@ -18,37 +18,48 @@ public class BinaryTree {
         b.right = e;
         c.left = f;
         c.right = g;
-
-        List<List<Integer>> arr =  betterlevelOrder(a);
-        System.out.println(arr);
+        // List<List<Integer>> arr =  betterlevelOrder(a);
+        // System.out.println(arr);
         // CurrlevelOrder(a, 3);
+        
+        // TreeNode ans = FindLeastCommonAncestor(a, f, g);
+        // System.out.println(ans.data);
+        // System.out.println(diameterOfTree(a));
+        // postOrder(a);
+        // System.out.println(arr);
+        // System.out.println(isBalancedTree(a));
+        System.out.println(finalFastBalancedTree(a));
+
     }
+    static ArrayList arr = new ArrayList<>();
 
     static void preOrder(TreeNode root) {
         if (root == null) {
             return;
         }
         System.out.println(root.data);
+        arr.add(root.data);
         preOrder(root.left);
-        preOrder(root.left);
+        preOrder(root.right);
     }
 
     static void postOrder(TreeNode root) {
         if (root == null) {
             return;
         }
-        preOrder(root.left);
-        preOrder(root.left);
+        postOrder(root.left);
+        postOrder(root.right);
         System.out.println(root.data);
+        arr.add(root.data);
     }
 
     static void inOrder(TreeNode root) {
         if (root == null) {
             return;
         }
-        preOrder(root.left);
+        inOrder(root.left);
         System.out.println(root.data);
-        preOrder(root.left);
+        inOrder(root.right);
     }
 
     static void CurrlevelOrder(TreeNode root, int level) {
@@ -108,7 +119,99 @@ public class BinaryTree {
         }
         return ans;
     }
+    
+    static TreeNode FindLeastCommonAncestor(TreeNode root, TreeNode a, TreeNode b){
+            if (root==null) return null;
+
+            if (root == a || root == b) return root;
+            TreeNode left = FindLeastCommonAncestor(root.left, a, b);
+            TreeNode right = FindLeastCommonAncestor(root.right, a, b);
+
+            if(left!=null && right != null) return root;
+
+            return left != null ? left : right;
+
     }
+    
+    static int diameterOfTree(TreeNode root) {
+        height(root);
+        return ans;
+    }
+    
+    static int ans=0;
+
+    static int height(TreeNode root){
+        if(root==null)return 0;
+        int lh=height(root.left);
+        int rh=height(root.right);
+        ans=Math.max(ans,rh+lh+1);
+        return 1 + Math.max(lh,rh);
+    }
+
+    static Boolean isBalancedTree(TreeNode root){
+        if(root==null) return true;
+        return Math.abs(heightTree(root.left) - heightTree(root.right)) <=1 && isBalancedTree(root.left)
+        && isBalancedTree(root.right);
+    }
+    
+    static ArrayList res = new ArrayList<>();
+
+    static ArrayList isBalancedFastTree(TreeNode root){
+        if (root == null){
+            res.add(true);
+            res.add(0);
+            return res;
+        }
+        ArrayList left = isBalancedFastTree(root.left);
+        ArrayList right = isBalancedFastTree(root.right);
+
+        boolean leftAns = (boolean) left.get(0);
+        boolean rightAns = (boolean) right.get(0);
+
+        boolean diff = Math.abs((int) left.get(1) - (int) right.get(1)) <=1;
+        ArrayList ans = new ArrayList<>();  
+
+        if (leftAns && rightAns && diff){
+            ans.add(true);
+        }
+        else{
+            ans.add(false);
+        }
+
+        ans.add(Math.max((int) left.get(1), (int) right.get(1)) + 1);
+
+        return ans;
+    }
+
+    static boolean finalFastBalancedTree(TreeNode root){
+        if(root== null) return true;
+        return (boolean) isBalancedFastTree(root).get(0);
+    }
+
+    static Boolean isSameTree(TreeNode p, TreeNode q){
+        if (p == null  && q == null){
+            return true;
+        }
+        if(p == null && q != null){
+            return false;
+        }
+        if(p != null && q == null){
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        boolean val = (p.data == q.data);
+
+        if(left && right && val){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
 
 
 class TreeNode {
