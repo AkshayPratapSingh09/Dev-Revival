fc1420a3991c3148713f5695d6f8cf1a8b42ff0d
Akshay <worklikeakshay@gmail.com>
2024-08-18 23:33:17 +0530

    Added Split Equal Sum

---
+++ Java/SplitEqualSum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SplitEqualSum {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        int sumArr[]= new int[arr.length];
+        int sum = 0;
+        for (int i=0; i<arr.length; i++){
+            sum += arr[i];
+            sumArr[i] = sum;
+        }
+        System.out.println(Arrays.toString(sumArr));
+        boolean flag = false;
+    
+        for (int i = 0; i < sumArr.length-1; i++) {
+            if (sumArr[i] == (sumArr[sumArr.length-1] - sumArr[i])){
+                System.out.println(arr[i]+ " " + arr[i+1]);
+                flag = true;
+            }
+        }
+        System.out.println(flag);
+    }
+}
