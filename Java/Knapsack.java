131b746f9fcca054096c073f9860694ab93a455e
Akshay <worklikeakshay@gmail.com>
2024-08-25 23:42:39 +0530

    Made Necessary Changes

--- Java/Knapsack.java
+++ Java/Knapsack.java
@@ -8,16 +8,8 @@ public class Knapsack {
         if (n == 0 || W == 0)
             return 0;
 
-        // If weight of the nth item is
-        // more than Knapsack capacity W,
-        // then this item cannot be included
-        // in the optimal solution
         if (wt[n - 1] > W)
             return knap(W, wt, val, n - 1);
-
-        // Return the maximum of two cases:
-        // (1) nth item included
-        // (2) not included
         else
             return Math.max(knap(W, wt, val, n - 1), 
              val[n - 1] + knap(W - wt[n-1], wt, val, n-1));
