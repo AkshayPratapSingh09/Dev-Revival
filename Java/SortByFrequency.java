57db11c868df0ed9cdd35af0b162c5ade6b784f5
Akshay <worklikeakshay@gmail.com>
2024-08-28 23:51:12 +0530

    Added Sort By Frequency

---
+++ Java/SortByFrequency.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SortByFrequency {
+    public static void main(String args[]){
+        int arr[] = {5, 5, 4, 6, 4};
+        TreeMap<Integer,Integer> has = new TreeMap<>();
+
+        for (int i : arr) {
+            if(!has.containsKey(i)){
+                has.put(i, 1);
+            } else {
+                has.put(i, has.get(i)+1);
+            }
+        }
+        System.out.println(has);
+    }
+}
