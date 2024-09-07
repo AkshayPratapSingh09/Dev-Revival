c489cce83df011141262be555893004272f27f42
Akshay <worklikeakshay@gmail.com>
2024-09-07 23:54:51 +0530

    Added more logic

--- Java/RotateString.java
+++ Java/RotateString.java
@@ -5,12 +5,13 @@ public class RotateString {
 
         String test = s + s;
         System.out.println(test);
-        // int len = s.length();
+        int len = s.length();
 
         for (int i=0;i<len;i++){
             if (test.charAt(i) == goal.charAt(0)){
                 if (test.substring(i, i+len).equals(goal)){
                     System.out.println("true");
+                    break;
                 }
             }
             System.out.println("This time : "+test.substring(i,i+len));
