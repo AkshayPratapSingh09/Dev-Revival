7d4940a0a99d428588f65a88399ab1a2217c17dd
Akshay <worklikeakshay@gmail.com>
2024-10-12 23:53:01 +0530

    Made Chnages

--- Java/Pair.java
+++ Java/Pair.java
@@ -15,11 +15,11 @@ public class CelebrityProblem {
         int m = mat.length;
         int n = mat[0].length;
         int ans = -1;
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
+        // for (int i=0; i<m; i++){
+        //     for (int j=0; j<n; j++){
                 
-            }
-        }
+        //     }
+        // }
         
         return -1;
     }
