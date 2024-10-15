4db9d62d0ca65c0ebe908291adcf1eead25f2fe1
Akshay <worklikeakshay@gmail.com>
2024-10-15 23:58:20 +0530

    Made Changes

--- Java/SortByFrequency.java
+++ Java/SortByFrequency.java
@@ -4,13 +4,13 @@ public class SortByFrequency {
         int arr[] = {5, 5, 4, 6, 4};
         TreeMap<Integer,Integer> has = new TreeMap<>();
 
-        for (int i : arr) {
-            if(!has.containsKey(i)){
-                has.put(i, 1);
-            } else {
-                has.put(i, has.get(i)+1);
-            }
-        }
+        // for (int i : arr) {
+        //     if(!has.containsKey(i)){
+        //         has.put(i, 1);
+        //     } else {
+        //         has.put(i, has.get(i)+1);
+        //     }
+        // }
         System.out.println(has);
     }
 }
