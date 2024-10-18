07386de8bde2f5a033815e6bdf919edaba6a815e
Akshay <worklikeakshay@gmail.com>
2024-10-18 23:57:18 +0530

    Added changes

--- Java/Strlen.java
+++ Java/Strlen.java
@@ -4,7 +4,7 @@ public class Strlen {
   public static void main(String[] args) {
     int[] arr ={1,2,4};
     String s = "Trial";
-    System.out.println(arr.length);
-    System.out.println(s.length());
+    // System.out.println(arr.length);
+    // System.out.println(s.length());
   }  
 }
