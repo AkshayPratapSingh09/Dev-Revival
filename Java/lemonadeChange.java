8357748ec0c945d96adc344fbb47a2a00e215b3f
Akshay <worklikeakshay@gmail.com>
2024-10-27 23:54:57 +0530

    Added

--- Java/lemonadeChange.java
+++ Java/lemonadeChange.java
@@ -7,26 +7,26 @@ public class lemonadeChange {
         // int[]  bills = {5,5,5,10,20};
         int[]  bills = {5,5,10,10,20};
         int change = 0;
-        for(int i=0; i<bills.length; i++){
-            if (bills[i] == 5){
-                System.out.println("This one is 5");
-                System.out.println("change is "+change);
-                change +=5;
-            }else{
-                System.out.println("This one is not 5 "+bills[i]);
-                System.out.println("change is "+change);
+        // for(int i=0; i<bills.length; i++){
+        //     if (bills[i] == 5){
+        //         System.out.println("This one is 5");
+        //         System.out.println("change is "+change);
+        //         change +=5;
+        //     }else{
+        //         System.out.println("This one is not 5 "+bills[i]);
+        //         System.out.println("change is "+change);
                 
-                int changeToReturn = bills[i]-5;
-                System.out.println("return to pay is "+changeToReturn);
-                if (change < changeToReturn){
-                    System.out.println(false);
-                }else{
-                    change -= changeToReturn;
-                    change += bills[i];
-                }
+        //         int changeToReturn = bills[i]-5;x
+        //         System.out.println("return to pay is "+changeToReturn);
+        //         if (change < changeToReturn){
+        //             System.out.println(false);
+        //         }else{
+        //             change -= changeToReturn;
+        //             change += bills[i];
+        //         }
                  
-            }
-        }
+        //     }
+        // }
         System.out.println(true);
     }
 }
