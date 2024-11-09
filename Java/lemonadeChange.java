d23c657914bc868519faf554da7972f943cb98b6
Akshay <worklikeakshay@gmail.com>
2024-11-09 23:59:32 +0530

    Made Chnages

--- Java/lemonadeChange.java
+++ Java/lemonadeChange.java
@@ -16,13 +16,13 @@ public class lemonadeChange {
         //         System.out.println("This one is not 5 "+bills[i]);
         // //         System.out.println("change is "+change);
                 
-        // //         int changeToReturn = bills[i]-5;x
-        // //         System.out.println("return to pay is "+changeToReturn);
-        // //         if (change < changeToReturn){
-        // //             System.out.println(false);
-        // //         }else{
-        // //             change -= changeToReturn;
-        // //             change += bills[i];
+        //         int changeToReturn = bills[i]-5;x
+        //         System.out.println("return to pay is "+changeToReturn);
+        //         if (change < changeToReturn){
+        //             System.out.println(false);
+        //         }else{
+        //             change -= changeToReturn;
+        //             change += bills[i];
         //         }
                  
         // //     }
