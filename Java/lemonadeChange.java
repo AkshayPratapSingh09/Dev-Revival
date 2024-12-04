e1350db8352a7ad3174c76d03b314b006573f78a
Akshay <worklikeakshay@gmail.com>
2024-12-04 23:40:18 +0530

    Added More

--- Java/lemonadeChange.java
+++ Java/lemonadeChange.java
@@ -9,20 +9,20 @@ public class lemonadeChange {
         int change = 0;
         for(int i=0; i<bills.length; i++){
             if (bills[i] == 5){
-                // System.out.println("This one is 5");
-                // System.out.println("change is "+change);
+                System.out.println("This one is 5");
+                System.out.println("change is "+change);
                 change +=5;
             }else{
-                // System.out.println("This one is not 5 "+bills[i]);
-                // System.out.println("change is "+change);
+                System.out.println("This one is not 5 "+bills[i]);
+                System.out.println("change is "+change);
                 
                 int changeToReturn = bills[i]-5;x
-                // System.out.println("return to pay is "+changeToReturn);
+                System.out.println("return to pay is "+changeToReturn);
                 if (change < changeToReturn){
                     System.out.println(false);
                 }else{
                     change -= changeToReturn;
-                    // change += bills[i];
+                    change += bills[i];
                 }
                  
             }
