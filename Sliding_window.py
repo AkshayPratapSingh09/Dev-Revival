e264b75cabbc443f97b676bccc8330ed976131c8
Akshay <worklikeakshay@gmail.com>
2024-12-23 23:46:17 +0530

    another One

--- Sliding_window.py
+++ Sliding_window.py
@@ -4,12 +4,12 @@
 left = 0
 right = len(arr)-1
 
-# i = 0
-# while i:
-#     if arr[i]
+i = 0
+while i:
+    if arr[i]
 
-# print(arr[left])
-# print(arr[right])
+print(arr[left])
+print(arr[right])
 
 while i<len(arr)-1:
     if arr[i]<5 and arr[i]>3:
