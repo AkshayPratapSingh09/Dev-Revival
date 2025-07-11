be30dda7a9989e3c536a4d202069382465094668
Akshay <worklikeakshay@gmail.com>
2025-07-11 22:34:32 +0530

    Fixed Few codes

--- Java/Recursion/revereseNums.java
+++ Java/Recursion/revereseNums.java
@@ -4,7 +4,7 @@ public class revereseNums {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7,8,9,10,11};
         reverse(nums, 0, nums.length);
-        System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(nums));
     };
     
     static void reverse(int[] nums, int i, int n){
