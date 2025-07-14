5b689c96dc379eea40396bd294ce42c578727e69
Akshay <worklikeakshay@gmail.com>
2025-07-14 23:51:47 +0530

    Fixed Few codes

--- Java/Recursion/PrintNameNtimes.java
+++ Java/Recursion/PrintNameNtimes.java
@@ -1,15 +1,15 @@
 
 public class PrintNameNtimes {
 public static void main(String[] args) {
-    // String s ="Hey!";
-    // printNTimes(s, 0);
+    String s ="Hey!";
+    printNTimes(s, 0);
 }
 
 static void printNTimes(String Name, int count){
-    if (count==5) return;
+    // if (count==5) return;
 
-    System.out.println(Name);
-    printNTimes(Name, count+1);
+    // System.out.println(Name);
+    // printNTimes(Name, count+1);
 
 }
     
