75358c57260395ef19b10e217099fe45732f884f
Akshay <worklikeakshay@gmail.com>
2025-07-23 23:49:30 +0530

    Started Word ladder

--- Java/Patterns/BFS/Word_Ladder.java
+++ Java/Patterns/BFS/Word_Ladder.java
@@ -1,5 +1,13 @@
 package BFS;
 
 public class Word_Ladder {
-    
+    public static void main(String[] args) {
+        System.out.println(FindAns());
+    }
+
+    public static Integer FindAns(){
+        return 1;
+    }
 }
+
+
