b906a38f6a94e8c88c8d25de9d133ff61f46a7b1
Akshay <worklikeakshay@gmail.com>
2025-07-24 22:53:29 +0530

    Added Fixed Coe

--- Java/Patterns/BFS/Word_Ladder.java
+++ Java/Patterns/BFS/Word_Ladder.java
@@ -3,6 +3,9 @@ package BFS;
 public class Word_Ladder {
     public static void main(String[] args) {
         System.out.println(FindAns());
+        String beginWord = "hit";
+        String endWord = "cog";
+        String[] wordList = {"hot","dot","dog","lot","log","cog"};
     }
 
     public static Integer FindAns(){
