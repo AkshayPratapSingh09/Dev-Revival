37cf4b1cd09f1a7ad6f47d1dd9d9e14138f3dcf7
Akshay <worklikeakshay@gmail.com>
2025-08-04 21:26:04 +0530

    Added more code

--- Java/Recursion/Sum_First_N_Number.java
+++ Java/Recursion/Sum_First_N_Number.java
@@ -1,15 +1,15 @@
-public class Sum_First_N_Number {
-    public static void main(String[] args) {
-        System.out.println(sumNum(10, 1, 0));
-        System.out.println(sumAltNum(10, 1));
+// public class Sum_First_N_Number {
+//     public static void main(String[] args) {
+//         System.out.println(sumNum(10, 1, 0));
+//         System.out.println(sumAltNum(10, 1));
 
-    }
+//     }
 
-    static int sumNum(int n,int k, int sum){
-        if (k == n) {
-            System.out.println("working");
-            return sum;
-        }
-            return sumNum(n, k+1, sum+k);        
-    }
-}
+//     static int sumNum(int n,int k, int sum){
+//         if (k == n) {
+//             System.out.println("working");
+//             return sum;
+//         }
+//             return sumNum(n, k+1, sum+k);        
+//     }
+// }
