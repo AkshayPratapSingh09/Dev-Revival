d46b044d315cb5e62cae3ddba3de1fe427fe5070
Akshay <worklikeakshay@gmail.com>
2025-08-21 23:46:14 +0530

    Improved Code

--- Java/AllPermutations.java
+++ Java/AllPermutations.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class AllPermutations {
     public void main(String[] args) {
-        String s="ABC";
-        ArrayList<String> ans = new ArrayList<>();
-        permute(0,s, ans);
-        System.out.println(ans);
+        // String s="ABC";
+        // ArrayList<String> ans = new ArrayList<>();
+        // permute(0,s, ans);
+        // System.out.println(ans);
     }
 
     public static String swap(String s,int a,int b)
