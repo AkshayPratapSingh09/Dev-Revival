a34de0c75cc1e1bf96ca449b806dec082865f1fb
Akshay <worklikeakshay@gmail.com>
2025-08-23 23:39:21 +0530

    Improved Code

--- Java/AllPermutations.java
+++ Java/AllPermutations.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class AllPermutations {
     public void main(String[] args) {
-        // String s="ABC";
-        // ArrayList<String> ans = new ArrayList<>();
-        // permute(0,s, ans);
-        // System.out.println(ans);
+        String s="ABC";
+        ArrayList<String> ans = new ArrayList<>();
+        permute(0,s, ans);
+        System.out.println(ans);
     }
 
     public static String swap(String s,int a,int b)
   {
     char ch[] = s.toCharArray();
     char p=ch[a];
-    ch[a]=ch[b];
-    ch[b]=p;
+    // ch[a]=ch[b];
+    // ch[b]=p;
     return String.valueOf(ch);
   }
     public static void permute(int index, String s,ArrayList<String> ans)
