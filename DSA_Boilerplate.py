59802041eb5b5437b3c60afa62240e22a983abf5
Akshay <worklikeakshay@gmail.com>
2025-08-29 23:57:27 +0530

    Improved Code

--- DSA_Boilerplate.py
+++ DSA_Boilerplate.py
@@ -1,46 +1,46 @@
-def BinarySearch(num,arr):
+# def BinarySearch(num,arr):
 
-    start = 0
-    end = len(arr)-1
+#     start = 0
+#     end = len(arr)-1
 
-    # mid  = (start + end)//2
+#     # mid  = (start + end)//2
 
-    while (start <= end):
-        print("Here",arr[start:end+1])
-        print(arr[mid])
+#     while (start <= end):
+#         print("Here",arr[start:end+1])
+#         print(arr[mid])
 
-        if arr[mid] == num:
-            return mid
+#         if arr[mid] == num:
+#             return mid
 
-        if (num> arr[mid]):
-            start = mid + 1
-        else:
-            end = mid - 1
+#         if (num> arr[mid]):
+#             # start = mid + 1
+#         else:
+#             end = mid - 1
 
-        mid = (start + end)//2
+#         mid = (start + end)//2
     
-    return -1
+#     return -1
 
-def InOrderTraversal(root):
-    if root==None:
-        return
-    InOrderTraversal(root.left)
-    print(root.data,end=" ")
-    InOrderTraversal(root.right)
+# def InOrderTraversal(root):
+#     if root==None:
+#         return
+#     InOrderTraversal(root.left)
+#     print(root.data,end=" ")
+#     InOrderTraversal(root.right)
 
-def PreOrderTraversal(root):
-    if root==None:
-        return
-    print(root.data, end=' ')
-    # print(root.data, end=' ')
-    # print(root.data, end=' ')
-    PreOrderTraversal(root.left)
-    PreOrderTraversal(root.right)
+# def PreOrderTraversal(root):
+#     if root==None:
+#         return
+#     print(root.data, end=' ')
+#     # print(root.data, end=' ')
+#     # print(root.data, end=' ')
+#     PreOrderTraversal(root.left)
+#     PreOrderTraversal(root.right)
 
-def PostOrderTraversal(root):
-    if root==None:
-        return
-    PostOrderTraversal(root.left)
-    PostOrderTraversal(root.right)
-    print(root.data,end=" ")
+# def PostOrderTraversal(root):
+#     if root==None:
+#         return
+#     PostOrderTraversal(root.left)
+#     PostOrderTraversal(root.right)
+#     print(root.data,end=" ")
 
