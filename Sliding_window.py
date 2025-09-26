9f3a4877cc265f53529369ba4742d93cc1a818af
Akshay <worklikeakshay@gmail.com>
2025-09-26 23:49:41 +0530

    Added some problem

--- Sliding_window.py
+++ Sliding_window.py
@@ -5,13 +5,13 @@ int[] arr = {1,2,3,4,5,6,7}
 left = 0
 right = len(arr)-1
 
-i = 0
-while i:
-    if arr[i]
+# i = 0
+# while i:
+#     if arr[i]
 
 print(arr[left])
 print(arr[right])
 
 while i<len(arr)-1:
 if arr[i]<5 and arr[i]>3:
-    # pass
+    pass
