4dbdfa7d56eb2c72d9418964a8b81e0cd921134a
Akshay <worklikeakshay@gmail.com>
2025-10-15 23:42:13 +0530

    Added More code

--- Java/Recursion/revereseNums.java
+++ Java/Recursion/revereseNums.java
@@ -15,6 +15,9 @@ public class revereseNums {
         int temp = nums[i];
         nums[i] = nums[n-i-1];
         nums[n-i-1] = temp;
+        int temp = nums[i];
+        nums[i] = nums[n-i-1];
+        nums[n-i-1] = temp;
 
         reverse(nums, i+1, n);
     }
