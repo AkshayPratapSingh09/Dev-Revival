8682a6a7c02a81d7adad7fb04fb9c97557158561
Akshay <worklikeakshay@gmail.com>
2025-10-19 22:38:50 +0530

    Added more code

--- Java/Patterns/Sliding_Window/Longest_Substring_Without_Repeating_Characters.java
+++ Java/Patterns/Sliding_Window/Longest_Substring_Without_Repeating_Characters.java
@@ -2,4 +2,7 @@ package Sliding_Window;
 
 public class Longest_Substring_Without_Repeating_Characters {
     
+    public public static void main(String[] args) {
+        System.out.println("Working");
+    }
 }
