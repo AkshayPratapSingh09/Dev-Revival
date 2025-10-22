8b0ffea315f5f011bd5703168e2cf85c96707269
Akshay <worklikeakshay@gmail.com>
2025-10-22 23:26:16 +0530

    Added more code

--- Java/Patterns/Dynamic_Programming/Climbing_Stairs.java
+++ Java/Patterns/Dynamic_Programming/Climbing_Stairs.java
@@ -1,5 +1,16 @@
 package Dynamic_Programming;
 
 public class Climbing_Stairs {
-    
+    public static void main(String[] args) {
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+        System.out.println("Print is working");
+    }
 }
