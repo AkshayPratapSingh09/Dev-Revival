dec03e283f7972cbbec0b80c684f562ec618e89b
Akshay <worklikeakshay@gmail.com>
2025-10-29 23:46:48 +0530

    Addded more code

--- Java/BinarySearch/secondProgram.java
+++ Java/BinarySearch/secondProgram.java
@@ -1,6 +1,11 @@
 public class secondProgram {
 public static void main(String args[]){
 System.out.println("Working");
+System.out.println("Working");
+System.out.println("Working");
+System.out.println("Working");
+System.out.println("Working");
+
 }
 
 }
