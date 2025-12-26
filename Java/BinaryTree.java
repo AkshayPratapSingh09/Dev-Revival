ef10d1d83ea1635c23577facc13c84cc255206e5
Akshay <worklikeakshay@gmail.com>
2025-12-26 22:01:17 +0530

    Added more code

--- Java/BinaryTree.java
+++ Java/BinaryTree.java
@@ -6,7 +6,7 @@ public class BinaryTree {
     public static void main(String[] args) {
          TreeNode a = new TreeNode(10);
          TreeNode b = new TreeNode(20);
-        // TreeNode c = new TreeNode(30);
+        TreeNode c = new TreeNode(30);
         // TreeNode d = new TreeNode(40);
         // TreeNode e = new TreeNode(50);
         // TreeNode f = new TreeNode(60);
